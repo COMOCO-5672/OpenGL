bdb2ff7426f76585d32208a779b601716f819ef4
COMOCO1 <775324623@qq.com>
2022-03-26 16:06:57 +0800

    update

--- OpenGL_1/main.cpp
+++ OpenGL_1/main.cpp
@@ -151,18 +151,6 @@ void init()
 
 void main()
 {
-    glm::mat4 trans(1.0f);
-    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0, 1, 0));
-    //0  0  1  0
-    //0  1  0  0
-    //-1 0  0  0
-    //0  0  0  1
-    float *p = glm::value_ptr(trans);
-    std::cout << typeid(p).name() << std::endl;
-    for (int i = 0; i < 16; i++) {
-        std::cout << *p << ' ';
-        p++;
-    }
     glfwInit();
 
     m_pWindow = glfwCreateWindow(800, 600, "opengl application", nullptr, nullptr);
